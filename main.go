a995e77b91435ac9897caff2300b58aee1842dfc
Joachim Hertaas <jhertaas@gmail.com>
2025-08-20 10:28:07 +0200

    removing and updating tasks

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"go-learning/db"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,7 +17,7 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("What would you like todo?")
-	fmt.Println("1. Check tasks \n2. Create a task \n3. Exit")
+	fmt.Println("1. Check tasks \n2. Create a task \n3. Update a task \n4. Delete a task \n5. Exit")
 	fmt.Println("Type the number of the option")
 	choice, _ := reader.ReadString('\n')
 	choice = strings.TrimSpace(choice)
@@ -27,9 +28,17 @@ func main() {
 		showTasks(ctx)
 	case "2":
 		if err := createTask(ctx, reader); err != nil {
-			log.Fatal("Failed to create task:", err)
+			log.Fatal("failed to create task:", err)
 		}
 	case "3":
+		if err := updateTask(ctx, reader); err != nil {
+			log.Fatal("failed to update task", err)
+		}
+	case "4":
+		if err := deleteTask(ctx, reader); err != nil {
+			log.Fatal("failed to delete task", err)
+		}
+	case "5":
 		fmt.Println("Program exited")
 		os.Exit(0)
 	default:
@@ -38,21 +47,22 @@ func main() {
 }
 
 func showTasks(ctx context.Context) {
-	rows, err := db.Conn.Query(ctx, "SELECT task, type, status FROM todo")
+	rows, err := db.Conn.Query(ctx, "SELECT id, task, type, status FROM todo")
 	if err != nil {
 		log.Fatal("Something went wrong", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
+		var id int
 		var task, taskType string
 		var status bool
 
-		err := rows.Scan(&task, &taskType, &status)
+		err := rows.Scan(&id, &task, &taskType, &status)
 		if err != nil {
-			log.Fatal("Something went wrong", err)
+			log.Fatal("something went wrong", err)
 		}
-		fmt.Println("Task:", task, "| Type:", taskType, "| Completed:", status)
+		fmt.Println("ID:", id, "| Task:", task, "| Type:", taskType, "| Completed:", status)
 	}
 }
 
@@ -73,5 +83,61 @@ func createTask(ctx context.Context, reader *bufio.Reader) error {
 	}
 
 	fmt.Println("Task created succesfully")
+	return nil
+}
+
+func updateTask(ctx context.Context, reader *bufio.Reader) error {
+	fmt.Print("Enter the task id: ")
+	edit, _ := reader.ReadString('\n')
+	edit = strings.TrimSpace(edit)
+	id, err := strconv.Atoi(edit)
+	if err != nil {
+		return fmt.Errorf("invalid id: %v", err)
+	}
+
+	fmt.Print("Mark as completed? (y/n): ")
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+
+	var newStatus bool
+	if choice == "y" {
+		newStatus = true
+	} else {
+		newStatus = false
+	}
+
+	_, err = db.Conn.Exec(ctx, "UPDATE todo SET status = $1 WHERE id = $2", newStatus, id)
+	if err != nil {
+		return fmt.Errorf("failed to update task: %v", err)
+	}
+
+	fmt.Println("Task updated successfully")
+	return nil
+}
+
+func deleteTask(ctx context.Context, reader *bufio.Reader) error {
+	
+	fmt.Println("Enter task id: ")
+	delete, _ := reader.ReadString('\n')
+	delete = strings.TrimSpace(delete)
+	id, err := strconv.Atoi(delete)
+	if err != nil {
+		return fmt.Errorf("invalid id: %v", err)
+	}
+
+	fmt.Print("Are you sure? (y/n)")
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+
+	if choice == "n"{
+		os.Exit(0)
+	}
+
+	_, err = db.Conn.Exec(ctx, "DELETE FROM todo WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Task deleted succesfully")
 	return nil
 }
